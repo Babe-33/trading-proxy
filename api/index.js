757449b65fef49ee5942c3ed587b321701d9872d
Babe-33 <bazire.b@hotmail.fr>
2026-04-06 13:39:25 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,13 +3,16 @@ export default async function handler(req, res) {
   let targetBase = "https://testnet.binance.vision";
   if (req.url.includes("discord.com")) targetBase = "https://discord.com";
   if (req.url.includes("mistral.ai")) targetBase = "https://api.mistral.ai";
-  const url = targetBase + req.url.replace("/api-proxy", "");
+  if (req.url.includes("cryptopanic.com")) targetBase = "https://cryptopanic.com";
+  const url = targetBase + req.url;
   try {
     const response = await fetch(url, {
       method: req.method,
       headers: {
-        ...req.headers,
-        host: new URL(targetBase).host // On change le host pour chaque destination
+        'Authorization': req.headers['authorization'] || '',
+        'X-MBX-APIKEY': req.headers['x-mbx-apikey'] || '',
+        'Content-Type': 'application/json',
+        'User-Agent': 'Mozilla/5.0'
       },
       body: req.method !== 'GET' && req.method !== 'HEAD' ? JSON.stringify(req.body) : undefined
     });
